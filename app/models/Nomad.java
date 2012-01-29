18ba430f016949c15a3083bafbde197f51dc405c
omj <jerihofreek@gmail.com>
2012-01-29 20:20:41 +0200

    git govno

---
+++ app/models/Nomad.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gene
+ * Date: 23.01.12
+ * Time: 22:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Nomad {
+}
