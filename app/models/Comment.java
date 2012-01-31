cdf856a92682a1377345182b17b130d8d90d507a
omj <jerihofreek@gmail.com>
2012-02-01 00:18:03 +0200

    added basic persistence

---
+++ app/models/Comment.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gene
+ * Date: 31.01.12
+ * Time: 23:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class Comment {
+}
--- app/models/User.java
+++ app/models/User.java
@@ -3,9 +3,9 @@ package models;
 /**
  * Created by IntelliJ IDEA.
  * User: Gene
- * Date: 23.01.12
- * Time: 22:37
+ * Date: 31.01.12
+ * Time: 23:23
  * To change this template use File | Settings | File Templates.
  */
-public class Nomad {
+public class User {
 }
