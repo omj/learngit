de100a6c0580fcb38a00874d92a1acb697833215
omj <jerihofreek@gmail.com>
2012-02-08 18:02:23 +0200

    first screen was built

---
+++ app/Bootstrap.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gene
+ * Date: 08.02.12
+ * Time: 17:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bootstrap {
+}
